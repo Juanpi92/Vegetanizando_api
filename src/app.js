5d09f6eda036d54f4be4a4ba8e3ad1c37a835c46
Juanpi92 <juan.urgelles92@gmail.com>
2023-08-29 16:48:23 -0300

    feat(app.js) Creating basic structure

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+//import { connectDB } from "./infra/db.js";
+import cors from "cors";
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+mongoose.set("strictQuery", false);
+
+app.use(
+  cors({
+    exposedHeaders: ["auth-token"],
+  })
+);
+
+app.get("/", async (req, res) => {
+  res.status(200).send({ message: "API is ready to go!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`API ready to use in -> http://localhost:${PORT}`);
+});
