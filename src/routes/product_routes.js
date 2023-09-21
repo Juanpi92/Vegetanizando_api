1dae09de577798a7e7e417461915ff0830ae861f
Juan Pablo Urgelles Pelier <113916693+Juanpi92@users.noreply.github.com>
2023-09-21 20:23:26 -0300

    Update product_routes.js

--- src/routes/product_routes.js
+++ src/routes/product_routes.js
@@ -5,7 +5,6 @@ import {
   DeleteObjectCommand,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { v4 as uuidv4 } from "uuid";
 import sharp from "sharp";
 import multer from "multer";
 import { validate } from "../authorization/auth.js";
@@ -388,7 +387,7 @@ export const ProductRoutes = (app) => {
         .toBuffer();
 
       //Send the image to s3
-      let id_photo = `${uuidv4()}-${req.file.originalname}`;
+      let id_photo = `${req.file.originalname}`;
       const params = {
         Bucket: process.env.BUCKET_NAME,
         Key: id_photo,
@@ -499,7 +498,7 @@ export const ProductRoutes = (app) => {
           .resize({ heigth: 600, width: 400, fit: "contain" })
           .toBuffer();
 
-        let id_photo = `${uuidv4()}-${req.file.originalname}`;
+        let id_photo = `${req.file.originalname}`;
         const params = {
           Bucket: process.env.BUCKET_NAME,
           Key: id_photo,
