2d01aa069fa700a8a7d205be0867361c409f36fb
Juanpi92 <juan.urgelles92@gmail.com>
2023-11-09 21:14:58 -0300

    feat(stripe-webhook): Creating webhook for stripe

---
+++ src/webhook/stripe_webhook.js
@@ -0,0 +1,43 @@
+import nodemailer from "nodemailer";
+export const StripeRoutes = (app) => {
+  app.post("/webhook_stripe", async (req, res) => {
+    try {
+      const message = `
+        <!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+
+            <title>Vegetanizando</title>
+        </head>
+        <body>
+            <h1>Nova compra detetada</h1>
+        </body>
+        </html>
+        `;
+      const event = req.body;
+      if (event.type === "payment_intent.succeeded") {
+        const paymentIntent = event.data.object;
+        console.log(paymentIntent);
+        const transport = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD_EMAIL,
+          },
+        });
+
+        const emailConfig = {
+          from: process.env.EMAIL,
+          to: process.env.EMAIL,
+          subject: "Nova compra detetada",
+          html: message,
+          text: `Nova compra detetada`,
+        };
+        const response = await transport.sendMail(emailConfig);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
